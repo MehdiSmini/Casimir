4f09ad0a7156da6ad48de8c67e6b63772fb0e2ba
M. Smini Mehdi <msmini@insa-07567>
2017-12-20 18:00:29 +0100

    sécurisation session

--- poo_chat/src/main/java/AccepterSession.java
+++ poo_chat/src/main/java/AccepterSession.java
@@ -1,11 +1,10 @@
 public class AccepterSession {
-    private MessageReseau mr = new MessageReseau();
 
     public void accepter_session(Session session){
         if(!User.sessions.containsKey(session.getPseudo())){
             System.out.println("Demande de sessions de la part de " + session.getPseudo());
             Session session1 = new Session(session.getPseudo_cible(),session.getPseudo(),true);
-            Main.mr.send_udp_packet("c" + User.getPseudo() + session1.getPseudo_cible(), User.agents_actifs.get(User.getPseudo()));
+            Main.mr.send_udp_packet("c" + User.getPseudo() + session1.getPseudo_cible(), User.agents_actifs.get(session1.getPseudo_cible()));
             //messages = get_messages_previous_session
             //session.setMessages(messages);
             User.add_session(session1);
